a71bc9a33360e346eca7ff08b55aedaa99e28322
KISHAN9675 <sainikia9@gmail.com>
2026-05-31 19:33:52 +0000

    done

---
+++ .devcontainer/01_basics/dataType.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+let name = "kishan"
+let age = 22
+let isLoggedIn = false
+
+// number=> 2 to power 53
+//string => ""
+// boolean=> true/false 
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+// object
+console.log( typeof age)
+                                                                                                                                                                                                           
---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,6 @@
+const accountmail = "kish@gmail"
+let accountid = 122121
+var accountPassword = "9273"
+
+console.log(accountPassword , accountmail)
+console.table([accountPassword, accountid,accountmail])
