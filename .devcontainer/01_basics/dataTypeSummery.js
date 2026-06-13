dd148d761fd613e1dc81d0588c1ec5dec17662f9
KISHAN9675 <sainikia9@gmail.com>
2026-06-13 17:29:48 +0000

    config

---
+++ .devcontainer/01_basics/dataTypeSummery.js
@@ -0,0 +1,31 @@
+// primitive =
+
+// 7 type : String, Number, Boolean, undefined, Symbol, BigInt
+
+const score = 100
+const  scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
+const bigNumber = 76783478644684n
+
+
+//reference ( Non primitve)
+// Array , Object , Functions
+
+const heros = ["shaktiman","naagraj", "doga"];
+let myObj={
+    name: "kishan",
+
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
